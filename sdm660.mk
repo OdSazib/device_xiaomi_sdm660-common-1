6b0895f91fad34836a937924fbd4a95eb6e11d98
OdSazib <odsazib@gmail.com>
2020-10-26 02:08:03 +0600

    sdm660-common: Time to wake up (Awaken)
    Signed-off-by: OdSazib <odsazib@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -395,6 +395,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(COMMON_PATH)/overlay \
+    $(COMMON_PATH)/overlay-awaken \
     $(COMMON_PATH)/overlay-lineage
 
 # RRO configuration
@@ -402,7 +403,6 @@ TARGET_USES_RRO := true
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(COMMON_PATH)/overlay-lineage/lineage-sdk \
-    $(COMMON_PATH)/overlay-lineage/packages/apps/Snap \
     $(COMMON_PATH)/overlay-system
 
 # Permissions
